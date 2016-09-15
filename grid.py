Fix masking and bugs to get ORAS4 <-> MOM remapping working.

grid.py
@@ -39,8 +39,8 @@ class Grid(object):
         self.description = description
 
         if mask is None:
-            # Default is to mask all, up to user to unmask.
-            self.mask = np.ones((self.num_levels,
+            # Default is all unmasked, up to user to mask.
+            self.mask = np.zeros((self.num_levels,
                                  self.num_lat_points, self.num_lon_points),
                                  dtype='int')
         else:
@@ -86,7 +86,57 @@ class Grid(object):
         self.clon_t = clon
         self.clat_t = clat
 
-    def write_scrip(self, filename, history=''):
+    def write_test_scrip(self, filename):
+        """
+        Write out SCRIP grid contents in a format which is easier to test.
+        """
+
+        f = nc.Dataset(filename, 'w')
+
+        x = self.x_t
+        y = self.y_t
+        clat = self.clat_t
+        clon = self.clon_t
+
+        f.createDimension('lats', self.num_lat_points)
+        f.createDimension('lons', self.num_lon_points)
+        f.createDimension('grid_corners', 4)
+        f.createDimension('grid_rank', 2)
+
+        center_lat = f.createVariable('center_lat', 'f8', ('lats', 'lons'))
+        center_lat.units = 'degrees'
+        center_lat[:] = y[:]
+
+        center_lon = f.createVariable('center_lon', 'f8', ('lats', 'lons'))
+        center_lon.units = 'degrees'
+        center_lon[:] = x[:]
+
+        imask = f.createVariable('mask', 'i4', ('lats', 'lons'))
+        imask.units = 'unitless'
+        # Invert the mask. SCRIP uses zero for points that do not
+        # participate.
+        if len(self.mask.shape) == 2:
+            imask[:] = np.invert(self.mask[:])
+        else:
+            imask[:] = np.invert(self.mask[0, :, :])
+
+        corner_lat = f.createVariable('corner_lat', 'f8',
+                                      ('lats', 'lons', 'grid_corners'))
+        corner_lat.units = 'degrees'
+        corner_lat[:] = clat[:]
+
+        corner_lon = f.createVariable('corner_lon', 'f8',
+                                      ('lats', 'lons', 'grid_corners'))
+        corner_lon.units = 'degrees'
+        corner_lon[:] = clon[:]
+
+        f.close()
+
+
+    def write_scrip(self, filename, mask=None, write_test_scrip=True, history=''):
+        """
+        Write out grid in SCRIP format.
+        """
 
         self.make_corners()
 
@@ -117,12 +167,18 @@ class Grid(object):
 
         imask = f.createVariable('grid_imask', 'i4', ('grid_size'))
         imask.units = 'unitless'
+
         # Invert the mask. SCRIP uses zero for points that do not
         # participate.
-        if len(self.mask.shape) == 2:
-            imask[:] = np.invert(self.mask[:]).flatten()
+        if mask is not None:
+            mask = mask
         else:
-            imask[:] = np.invert(self.mask[0, :, :]).flatten()
+            mask = self.mask
+
+        if len(mask.shape) == 2:
+            imask[:] = np.invert(mask[:]).flatten()
+        else:
+            imask[:] = np.invert(mask[0, :, :]).flatten()
 
         corner_lat = f.createVariable('grid_corner_lat', 'f8',
                                       ('grid_size', 'grid_corners'))
@@ -137,3 +193,7 @@ class Grid(object):
         f.title = self.description
         f.history = history
         f.close()
+
+        if write_test_scrip:
+            self.write_test_scrip(filename + '_test')
+            

makeic.py
@@ -44,7 +44,7 @@ def regrid_columns(data, src_grid, dest_grid):
 
     # Create masked array of the correct shape.
     tmp = np.zeros((len(dest_grid.z), data.shape[1], data.shape[2]))
-    new_data = np.ma.array(tmp, mask=np.ones_like(tmp))
+    new_data = np.ma.array(tmp, mask=np.ones_like(tmp), copy=True)
 
     # Iterate through columns and regrid each.
     for lat in range(data.shape[1]):
@@ -73,7 +73,7 @@ def extend_obs(var, obs_grid, global_grid, arctic_filler=None):
     # Create masked array of the correct shape.
     tmp = np.zeros((global_grid.num_levels, global_grid.num_lat_points,
                     global_grid.num_lon_points))
-    new_data = np.ma.array(tmp, mask=global_grid.mask)
+    new_data = np.ma.array(tmp, mask=global_grid.mask, copy=True)
 
     # Drop obs data into new grid at correct location
     lat_min_idx = find_nearest_index(global_grid.y_t[:, 0], np.min(obs_grid.y_t[:]))
@@ -93,7 +93,6 @@ def extend_obs(var, obs_grid, global_grid, arctic_filler=None):
         new_data[l, :, :] = tmp[:, :]
 
     return new_data
-
 
 def fill_arctic(obs_data, global_data, obs_grid, global_grid):
     """
@@ -249,9 +248,10 @@ def main():
     msalt = np.ndarray((model_grid.num_levels, model_grid.num_lat_points,
                       model_grid.num_lon_points))
 
-    # Write the source and destination grids out in SCRIP format.
+    # Write the source and destination grids out in SCRIP format. We override
+    # the mask to use the data mask rather than the grid mask.
     _, global_grid_scrip = tempfile.mkstemp(suffix='.nc')
-    global_grid.write_scrip(global_grid_scrip)
+    global_grid.write_scrip(global_grid_scrip, gtemp.mask)
     _, model_grid_scrip = tempfile.mkstemp(suffix='.nc')
     model_grid.write_scrip(model_grid_scrip)
 

tripolar_grid.py
@@ -28,13 +28,16 @@ class TripolarGrid(Grid):
             x_t[:new_rows, :] = np.stack([tripolar_grid.x_t[0, :]]*new_rows)
 
             y_t[new_rows:, :] = tripolar_grid.y_t[:]
-            for n in range(new_rows-1, 0, -1):
+            for n in range(new_rows-1, -1, -1):
                 y_t[n, :] = y_t[n+1, :] - dy
+
+            tripolar_mask = np.zeros_like(tripolar_grid.mask, dtype=bool)
+            tripolar_mask[tripolar_grid.mask[:] == 0.0] = True
 
             # Drop the mask in, with new rows being masked by default.
             mask = np.ndarray((levels.shape[0], y_t.shape[0], y_t.shape[1]), dtype=bool)
             mask[:] = True
-            mask[levels.shape[0]-tripolar_grid.mask.shape[0]:, new_rows:, :] = tripolar_grid.mask[:]
+            mask[levels.shape[0]-tripolar_grid.mask.shape[0]:, new_rows:, :] = tripolar_mask[:]
         else:
             x_t = tripolar_grid.x_t[:]
             y_t = tripolar_grid.x_y[:]
@@ -55,11 +58,11 @@ class TripolarGrid(Grid):
 
         # Need to extend South
         dy_half[0, 1:] = dy_half[1, 1:]
-        dx_half[0, 1:] = dy_half[1, 1:]
+        dx_half[0, 1:] = dx_half[1, 1:]
 
         # and West
         dy_half[:, 0] = dy_half[:, 1]
-        dx_half[:, 0] = dy_half[:, 1]
+        dx_half[:, 0] = dx_half[:, 1]
 
         clon = np.empty((self.num_lat_points, self.num_lon_points, 4))
         clon[:] = np.NAN