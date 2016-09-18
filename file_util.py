57bc48687ad1b0b20ee7a7aac425465a1d9bfe02
Nicholas Hannah <nchlshnnh@gmail.com>
2016-09-18 13:11:57 +1000

    Date formatting.

--- file_util.py
+++ file_util.py
@@ -39,7 +39,7 @@ def create_mom_output(ocean_grid, filename, start_date, history):
 
     time = f.createVariable('time', 'f8', ('time'))
     time.long_name = 'time'
-    time.units = "days since {}-{}-01 00:00:00".format(start_date.year, start_date.month)
+    time.units = "days since {}-{}-01 00:00:00".format(str(start_date.year).zfill(4), start_date.month)
     time.cartesian_axis = "T"
     time.calendar_type = "GREGORIAN"
     time.calendar = "GREGORIAN"
@@ -83,7 +83,7 @@ def create_nemo_output(ocean_grid, filename, start_date, history):
 
     time = f.createVariable('time_counter', 'f8', ('time_counter'))
     time.long_name = 'time'
-    time.units = "days since {}-{}-01 00:00:00".format(start_date.year, start_date.month)
+    time.units = "days since {}-{}-01 00:00:00".format(str(start_date.year).zfill(4), start_date.month)
     time.cartesian_axis = "T"
 
     f.close()
